8c336a30e808e5da49f7a90504e490355157228a
hawken <hawken@thehawken.org>
2015-08-26 13:33:54 +0200

    remove binaries and solve oppg3

--- excercises/week1/oppg3.pas
+++ excercises/week1/oppg3.pas
@@ -1,20 +1,36 @@
 Program Fib;
 
-var n: Integer := 40;
+var n: Integer;
 
-function fib1: Integer;
+function fib1(x: Integer): Integer;
+var f1: Integer; f2: Integer; f3: Integer; i: Integer;
 begin
-	writeln('fib1 test');
+	f1 := 0; f2 := 1;
+	i := x;
+	while i > 0 do begin
+		f3 := f1+f2;
+
+		f1 := f2;
+		f2 := f3;
+
+		i := i-1;
+	end;
+	fib1 := f1;
 end; {fib1}
 
-function fib2: Integer;
+function fib2(x: Integer): Integer;
 begin
-	writeln('fib2 test');
-end; {fin2}
+	if x <= 2 then
+		fib2 := 1
+	else
+		fib2 := fib2(x-2) + fib2(x-1);
+end; {fib2}
 
 {function main: }
 
 begin
-	writeln('fib1(' + n + ') =' + fib1(n) , eol);
+	n := 40;
+	write('fib1(', n, ') =', fib1(n), eol);
+	write('fib2(', n, ') =', fib2(n), eol);
 end.
 
